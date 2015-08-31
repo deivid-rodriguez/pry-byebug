f6a8864262d9f1847b857da6cc13e7d91fdb66da
David Rodríguez <deivid.rodriguez@gmail.com>
2015-08-31 20:30:01 -0300

    Get rid of remote stuff until properly implemented

--- lib/pry-byebug/base.rb
+++
@@ -1,7 +0,0 @@
-#
-# Main container module for Pry-Byebug functionality
-#
-module PryByebug
-  # Reference to currently running pry-remote server. Used by the processor.
-  attr_accessor :current_remote_server
-end
--- test/pry_remote_ext_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class PryRemoteExtTest < MiniTest::Spec
-end
