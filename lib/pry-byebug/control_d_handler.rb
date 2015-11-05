860e83ae555f6b616ebcb65fa01dd0295afab231
David Rodríguez <deivid.rodriguez@gmail.com>
2015-11-05 18:04:44 +0100

    Fix overcommit offense

--- lib/pry-byebug/control_d_handler.rb
+++ lib/pry-byebug/control_d_handler.rb
@@ -1,7 +1,7 @@
 original_handler = Pry.config.control_d_handler
 
-Pry.config.control_d_handler = proc do |eval_string, _pry_|
+Pry.config.control_d_handler = proc do |eval_string, pry_|
   Byebug.stop if Byebug.stoppable?
 
-  original_handler.call(eval_string, _pry_)
+  original_handler.call(eval_string, pry_)
 end
