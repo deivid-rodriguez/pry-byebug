fac62c330394553347929e7fb4339a672a849dc3
David Rodríguez <deivid.rodriguez@gmail.com>
2016-11-22 12:49:18 -0200

    Bump codeclimate-test-reporter dev dependency

--- test/support/coverage.rb
+++ test/support/coverage.rb
@@ -1,17 +1,10 @@
 #
-# Starts code coverage tracking. If running on CI, use codeclimate's wrapper to
-# report results to them.
+# Starts code coverage tracking.
 #
 def start_coverage_tracking
   require 'simplecov'
   SimpleCov.add_filter 'test'
-
-  if ENV['CI']
-    require 'codeclimate-test-reporter'
-    CodeClimate::TestReporter.start
-  else
-    SimpleCov.start
-  end
+  SimpleCov.start
 end
 
 start_coverage_tracking
