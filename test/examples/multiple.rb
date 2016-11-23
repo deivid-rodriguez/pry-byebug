a8243c4aba27b03644957e2c8cdcb45ac79d7c96
Max Melentiev <melentievm@gmail.com>
2016-11-23 06:31:34 +0300

    Don't start byebug after disable-pry

---
+++ test/examples/multiple.rb
@@ -0,0 +1,3 @@
+5.times do
+  binding.pry
+end
--- test/pry_ext_test.rb
+++ test/pry_ext_test.rb
@@ -1,4 +1,23 @@
 require 'test_helper'
 
 class PryExtTest < MiniTest::Spec
+  let(:output) { StringIO.new }
+  let(:input) { InputTester.new }
+
+  describe 'when disable-pry called' do
+    subject { redirect_pry_io(input, output) { load test_file('multiple') } }
+    before { input.add 'disable-pry' }
+    after { ENV.delete('DISABLE_PRY') }
+
+    it "keeps pry's default behaviour" do
+      subject
+      assert_equal 1, output.string.scan(/binding\.pry/).size
+    end
+
+    it 'does not start byebug' do
+      Byebug.stop
+      subject
+      assert !Byebug.started?
+    end
+  end
 end
