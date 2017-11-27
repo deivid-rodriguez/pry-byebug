c455b32dfc58f6c46fb63e06942935c0236bb37f
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2017-11-28 01:34:44 +0900

    Allow other threads like pry (#142)

---
+++ test/examples/echo_thread.rb
@@ -0,0 +1,12 @@
+require 'socket'
+
+client, server = Socket.pair(Socket::AF_UNIX, Socket::SOCK_STREAM)
+
+Thread.new do
+  while (line = server.readline)
+    server.write(line)
+  end
+end
+
+binding.pry
+client
---
+++ test/thread_lock_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+require 'timeout'
+
+class ThreadLockTest < MiniTest::Spec
+  let(:output) { StringIO.new }
+  let(:input) { InputTester.new }
+
+  describe "when there's another thread" do
+    before do
+      input.add 'client.puts("Hello")'
+      input.add 'IO.select([client], [], [], 1) && client.readline'
+
+      redirect_pry_io(input, output) { load test_file('echo_thread') }
+    end
+
+    it "another thread isn't locked" do
+      assert_equal "=> \"Hello\\n\"\n", output.string.lines.last
+    end
+  end
+end
