a8123c1ae705da8be6cd556622dfc58cbabec2bf
Damien Robert <damien.olivier.robert+git@gmail.com>
2018-07-27 13:13:14 +0200

    Allow to pass options to Pry.start
    Adapted from JoshCheek code at
    https://github.com/deivid-rodriguez/pry-byebug/issues/45
    
    This fixes https://github.com/ConradIrwin/pry-rescue/issues/71

--- lib/pry-byebug/pry_ext.rb
+++ lib/pry-byebug/pry_ext.rb
@@ -5,7 +5,7 @@ class << Pry
 
   def start_with_pry_byebug(target = TOPLEVEL_BINDING, options = {})
     if target.is_a?(Binding) && PryByebug.file_context?(target)
-      Byebug::PryProcessor.start unless ENV["DISABLE_PRY"]
+      Byebug::PryProcessor.start(target, options) unless ENV["DISABLE_PRY"]
     else
       # No need for the tracer unless we have a file context to step through
       start_without_pry_byebug(target, options)
