702264f36608db339e2f9905e5b161c3c60d926c
Ali Sepehri <ali.sepehri.kh@gmail.com>
2022-08-16 16:59:40 +0200

    Bring conditions back to start with pry-byebug

--- lib/pry-byebug/pry_ext.rb
+++ lib/pry-byebug/pry_ext.rb
@@ -5,8 +5,15 @@ require "byebug/processors/pry_processor"
 class << Pry::REPL
   alias start_without_pry_byebug start
 
-  def start_with_pry_byebug(_ = {})
-    Byebug::PryProcessor.start unless ENV["DISABLE_PRY"]
+  def start_with_pry_byebug(options = {})
+    target = options[:target]
+
+    if target.is_a?(Binding) && PryByebug.file_context?(target)
+      Byebug::PryProcessor.start unless ENV["DISABLE_PRY"]
+    else
+      # No need for the tracer unless we have a file context to step through
+      start_without_pry_byebug(options)
+    end
   end
 
   alias start start_with_pry_byebug
